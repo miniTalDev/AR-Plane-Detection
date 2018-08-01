6d8baae1f129e31f5ba900074bc00ff2e1288fc4
miniDevOn <nasir522004@gmail.com>
2018-08-01 14:32:43 +0530

    Plane detection

--- GrassOnBalcony/ViewController.swift
+++ GrassOnBalcony/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var grids = [Grid]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,11 +25,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        // Add a touch event
+        let gestureRecognizer = UITapGestureRecognizer(target: self,
+                                                       action: #selector(tapped))
+        sceneView.addGestureRecognizer(gestureRecognizer)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +44,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -76,5 +87,48 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    // 1. When a plane detection happened
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+//        let grid = Grid(anchor: anchor as! ARPlaneAnchor)
+//        self.grids.append(grid)
+//        node.addChildNode(grid)
+    }
+    // 2. When the detected plane got updated
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+//        let grid = self.grids.filter { grid in
+//            return grid.anchor.identifier == anchor.identifier
+//            }.first
+//
+//        guard let foundGrid = grid else {
+//            return
+//        }
+//
+//        foundGrid.update(anchor: anchor as! ARPlaneAnchor)
+    }
+    
+    @objc func tapped(gesture: UITapGestureRecognizer) {
+        // Get exact position where touch happened on screen of iPhone (2D coordinate)
+        let touchPosition = gesture.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchPosition, types: .existingPlaneUsingExtent)
+        
+        if !hitTestResult.isEmpty {
+            guard let hitResult = hitTestResult.first else {
+                return
+            }
+            addGrass(hitTestResult: hitResult)
+        }
+    }
+    
+    func addGrass(hitTestResult: ARHitTestResult) {
+        let scene = SCNScene(named: "art.scnassets/mouthshark.dae")
+        let node = scene?.rootNode.childNode(withName: "shark", recursively: true)
+        node?.scale = SCNVector3(0.05, 0.05, 0.05)
+        node?.position = SCNVector3(hitTestResult.worldTransform.columns.3.x,
+                                         hitTestResult.worldTransform.columns.3.y,
+                                         hitTestResult.worldTransform.columns.3.z
+        )
+        self.sceneView.scene.rootNode.addChildNode(node!)
     }
 }
